fa47ecee1ac0bd730e5240aa7c34cc47570d21a4
Rex Soriano <awesomerex@gmail.com>
2015-10-22 13:11:57 -0400

    shakey eliah but merged movement with boss rendering

--- game.js
+++ game.js
@@ -6,13 +6,6 @@ var gameMaker = require("./gamemaker.js");
 var game = gameMaker.game;
 
 
-function centerText(context, text, offsetX, offsetY) {
-	var w = context.measureText(text).width;
-	var x = offsetX + (canvas.width / 2) - (w / 2) | 0;
-	var y = offsetY | 0;
-	context.fillText(text, x, y);
-}
-
 function createEntity(x, y, width, height, color){
 	var entity = new Splat.Entity(x,y,width,height);
 	entity.color = color;
@@ -54,22 +47,70 @@ function createPlayer(x, y, width, height){
 	console.log(particles);
 
 
-game.scenes.add("title1", new Splat.Scene(canvas, function() {
+game.scenes.add("title", new Splat.Scene(canvas, function() {
 	// initialization
 	var scene = this;
-	var cloud = game.animations.get("cloud");
 
-	scene.cloud = new Splat.AnimatedEntity(0,0, canvas.width, canvas.height, cloud, 0, 0);
-	scene.cloud2 = new Splat.AnimatedEntity(400,0, canvas.width, canvas.height, cloud, 0, 0);
 	scene.player = createEntity(100,100,50,50,"orange");
 	//scene.camera = new Splat.EntityBoxCamera(scene.player, canvas.width - 200, canvas.height + 100, canvas.width /2, canvas.height/2);
 	scene.camera = new Splat.Camera(0,0,384, 224);
 	scene.camera.vx = 0.1;
 	scene.player.vx = 0.1;
 
+
+	// initialization
+	particles.reset();
+	particles.gravity = 0;
+	particles.maxAge = 1000;
+
+	scene.bullets = [];
+	scene.seatile = game.animations.get("sea-tile");
+	var cloud = game.animations.get("cloud");
+	var hydra = game.animations.get("hydra");
+	var hydraFire = function(){
+		var bullet = new Splat.Entity(this.x, this.y, 1, 1);
+		bullet.targetx = scene.player.x + scene.player.width/2;
+		bullet.targety = scene.player.y + scene.player.height/2;
+		bullet.speed = 1;
+		bullet.distance =  Math.sqrt( Math.pow((bullet.targetx - bullet.x), 2) + Math.pow((bullet.targety - bullet.y),2));
+		bullet.speedx = Math.abs(bullet.targetx - bullet.x)/bullet.distance * bullet.speed;
+		bullet.speedy = Math.abs(bullet.targety - bullet.y)/bullet.distance * bullet.speed;
+		if (bullet.targetx - bullet.x < 0){
+			bullet.speedx *= -1;
+		}
+		if (bullet.targety - bullet.y < 0){
+			bullet.speedy *= -1;
+		}
+		bullet.move = function(){
+			this.x += this.speedx;
+			this.y += this.speedy;
+		};
+		scene.bullets.push(bullet);
+	};
+
+	scene.cloud = new Splat.AnimatedEntity(0,0, canvas.width, canvas.height, cloud, 0, 0);
+	scene.cloud2 = new Splat.AnimatedEntity(400,0, canvas.width, canvas.height, cloud, 0, 0);
+	scene.player = createPlayer(100,100,50,50);
+	scene.camera = new Splat.Camera(0,0,384, 224);
+	scene.arrow = game.animations.get("eliya_arrow");
+	scene.hydra = new Splat.AnimatedEntity(400, 20, hydra.width, hydra.height , hydra, 0, 0);
+	scene.hydra.spawner1 = new Splat.Entity(scene.hydra.x + 24, scene.hydra.y+75, 0, 0);
+	scene.hydra.spawner1.fire = hydraFire;
+	scene.hydra.spawner2 = new Splat.Entity(scene.hydra.x + 70, scene.hydra.y+28, 0, 0);
+	scene.hydra.spawner2.fire = hydraFire;
+	scene.hydra.spawner3 = new Splat.Entity(scene.hydra.x + 117, scene.hydra.y+73, 0, 0);
+	scene.hydra.spawner3.fire = hydraFire;
+
+	scene.timers.hydraFire = new Splat.Timer(undefined, 5000, function(){
+		scene.hydra.spawner1.fire();
+		this.reset();
+		this.start();
+	});
+	scene.timers.hydraFire.start();
+
 }, function(elapsedMillis) {
 	// simulation
-
+	var scene = this;
 	// set the boundary for the scrolling camera
 
 	if(this.camera.x >= 200){
@@ -89,65 +130,103 @@ game.scenes.add("title1", new Splat.Scene(canvas, function() {
 	//console.log(this.player.x - this.camera.x);
 	this.player.vx = 0;
 	this.player.vy = 0;
+	
+	// set the boundary for the scrolling camera
 	if (game.keyboard.isPressed("left")) {
-		this.player.vx = -0.2;
+		scene.player.vx = -0.2;
 	}
 	if (game.keyboard.isPressed("right")) {
-		this.player.vx = 0.2;
+		scene.player.vx = 0.2;
 	}
 	if (game.keyboard.isPressed("up")) {
-		this.player.vy = -0.2;
+		scene.player.vy = -0.2;
 	}
 	if (game.keyboard.isPressed("down")) {
-		this.player.vy = 0.2;
+		scene.player.vy = 0.2;
 	}
+
+	scene.player.move(elapsedMillis);
+	scene.hydra.move(elapsedMillis);
 
 	//control the camera
 	if (game.keyboard.isPressed("o")){
-		this.camera.x += 0.9;
+		scene.camera.x += 0.9;
 	}
 
 	if (game.keyboard.isPressed("p")){
-		this.camera.x -= 0.9;
+		scene.camera.x -= 0.9;
 	}
 
-	this.player.move(elapsedMillis);
+	if (game.mouse.consumePressed(0)){
+		console.log("x:" + game.mouse.x, "y:" + game.mouse.y);
+	}
+
+	if (game.mouse.consumePressed(2)){
+		scene.hydra.spawner1.fire();
+		scene.hydra.spawner2.fire();
+		scene.hydra.spawner3.fire();
+	}
 
 	// player boundaries in relation to the camera
-	if (this.player.x <= this.camera.x){
-		this.player.x = this.camera.x;
+	if (scene.player.x <= scene.camera.x){
+		scene.player.x = scene.camera.x;
 	}
-	if (this.player.x >= this.camera.x + this.camera.width - this.player.width){
-		this.player.x = this.camera.x + this.camera.width - this.player.width;
+	if (scene.player.x >= scene.camera.x + scene.camera.width - scene.player.width){
+		scene.player.x = scene.camera.x + scene.camera.width - scene.player.width;
 	}
 	//keep player inbetween top and bottom of screen
-	if (this.player.y <= 0){
-		this.player.y = 0;
+	if (scene.player.y <= 0){
+		scene.player.y = 0;
 	}
-	if (this.player.y >= canvas.height - this.player.height){
-		this.player.y = canvas.height - this.player.height;
+	if (scene.player.y >= canvas.height - scene.player.height){
+		scene.player.y = canvas.height - scene.player.height;
+	}
+	
+	//projectile management
+	for(var x = 0; x < scene.bullets.length; x++){
+		scene.bullets[x].move();
+		if(scene.bullets[x].y > scene.camera.height || 
+		   scene.bullets[x].y < 0 || scene.bullets[x].x < scene.camera.x || 
+		   scene.bullets[x].x > scene.camera.x + scene.camera.width){
+			this.bullets.splice(x,1);
+		}
 	}
 
-	console.log(this.camera.x);
+	//collision detection with player
+	for(x = 0; x < scene.bullets.length; x++){
+		if(scene.bullets[x].collides(scene.player)){
+			console.log("hit!");
+			scene.bullets.splice(x,1);
+		}
+	}
+
+		particles.move(elapsedMillis);
+
+
 
 }, function(context) {
 	// draw
+	this.player.draw(context);
+
+	var scene = this;
 	context.clearRect(this.camera.x, this.camera.y , canvas.width, canvas.height);
 
 	context.fillStyle = "#092227";
 	context.fillRect(0, 0, canvas.width * 200, canvas.height + 100);
 
 	context.fillStyle = "#fff";
-	context.font = "18px helvetica";
-	centerText(context, "Blank SplatJS Project", 0, canvas.height / 2 - 13);
 
-	this.cloud.draw(context);
-	this.cloud2.draw(context);
-	this.player.draw(context);
+	scene.player.draw(context);
+	scene.hydra.draw(context);
+	for(var x=0 ; x< scene.bullets.length; x++){
+		scene.bullets[x].draw(context);
+		particles.add(3, scene.bullets[x].x, scene.bullets[x].y, 0, "circle");
+	}
+	particles.draw(context);
 
 
 }));
-
+/*
 game.scenes.add("title", new Splat.Scene(canvas, function() {
 	// initialization
 	particles.reset();
@@ -308,5 +387,5 @@ game.scenes.add("title", new Splat.Scene(canvas, function() {
 	}
 	particles.draw(context);
 }));
-
+*/
 game.scenes.switchTo("loading");
